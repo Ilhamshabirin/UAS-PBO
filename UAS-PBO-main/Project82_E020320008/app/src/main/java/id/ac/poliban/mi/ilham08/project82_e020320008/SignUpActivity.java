aac4cfd0169d419c5cb5df027c6b41237bb3020b
Ilhamshabirin <ilhamshbrn@gmail.com>
2023-01-15 21:28:41 -0800

    first commit

---
+++ UAS-PBO-main/Project82_E020320008/app/src/main/java/id/ac/poliban/mi/ilham08/project82_e020320008/SignUpActivity.java
@@ -0,0 +1,14 @@
+package id.ac.poliban.mi.ilham08.project82_e020320008;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
